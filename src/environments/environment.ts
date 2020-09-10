754fadedf48a693d234b7361c184c400c11d232f
Samson <samsonudo@yahoo.com>
2020-09-10 12:35:28 +0100

    Update API Keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: false,
-  searchKey: 'AIzaSyCI3b8rwOo3ZPRMU-fV3hTXns8fxY0VKhM',
+  searchKey: 'Replace with Real Value',
   firebaseConfig: {
-    apiKey: "AIzaSyCI3b8rwOo3ZPRMU-fV3hTXns8fxY0VKhM",
-    authDomain: "amber-sam.firebaseapp.com",
-    databaseURL: "https://amber-sam.firebaseio.com",
-    projectId: "amber-sam",
-    storageBucket: "amber-sam.appspot.com",
-    messagingSenderId: "447571884274",
-    appId: "1:447571884274:web:6f847e0080ca97e5cf786b",
-    measurementId: "G-2FNBYRT5QT"
+    apiKey: "Replace with Real Value",
+    authDomain: "Replace with Real Value",
+    databaseURL: "Replace with Real Value",
+    projectId: "Replace with Real Value",
+    storageBucket: "Replace with Real Value",
+    messagingSenderId: "Replace with Real Value",
+    appId: "Replace with Real Value",
+    measurementId: "Replace with Real Value"
   }
 };
 
