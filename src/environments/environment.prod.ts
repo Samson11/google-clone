b87a342106d15cc59c396076652e90e62f50bf23
Samson <samsonudo@yahoo.com>
2020-09-10 12:31:14 +0100

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  searchKey: 'AIzaSyCI3b8rwOo3ZPRMU-fV3hTXns8fxY0VKhM',
+  firebaseConfig: {
+    apiKey: "AIzaSyCI3b8rwOo3ZPRMU-fV3hTXns8fxY0VKhM",
+    authDomain: "amber-sam.firebaseapp.com",
+    databaseURL: "https://amber-sam.firebaseio.com",
+    projectId: "amber-sam",
+    storageBucket: "amber-sam.appspot.com",
+    messagingSenderId: "447571884274",
+    appId: "1:447571884274:web:6f847e0080ca97e5cf786b",
+    measurementId: "G-2FNBYRT5QT"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  searchKey: 'AIzaSyCI3b8rwOo3ZPRMU-fV3hTXns8fxY0VKhM',
+  firebaseConfig: {
+    apiKey: "AIzaSyCI3b8rwOo3ZPRMU-fV3hTXns8fxY0VKhM",
+    authDomain: "amber-sam.firebaseapp.com",
+    databaseURL: "https://amber-sam.firebaseio.com",
+    projectId: "amber-sam",
+    storageBucket: "amber-sam.appspot.com",
+    messagingSenderId: "447571884274",
+    appId: "1:447571884274:web:6f847e0080ca97e5cf786b",
+    measurementId: "G-2FNBYRT5QT"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
